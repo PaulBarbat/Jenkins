2109cb8ef7077c07eea0046db2f228b7c431d59a
Paul Barbat <Paul.Barbat@Siriusxm.com>
2025-06-18 16:09:03 +0300

    Added build number to the executable and added logic to put executable on s3 bucket

--- jobs/build-cpp-66.groovy
+++ jobs/build-cpp-66.groovy
@@ -36,6 +36,24 @@ pipeline {
                 '''
             }
         }
+
+        stage('Upload to S3'){
+            steps{
+                script{
+                    def executable_path = sh(script: "ls build/Card_Game_66-*", returnStdout: true).trim()
+                    def executable = executable_path.split('/').last()
+                    def executable_with_build_number = "${executable}-${env.BUILD_NUMBER}"
+                    withAWS(region: 'eu-central-1', credentials: 'bd77ee04-1eed-4e13-ad2b-b4ad37857124') {
+                    sh '''
+                    ls -ll
+                    pwd
+                    aws s3 cp ${executable_with_build_number} s3://card-game-66-personal-bucket/builds/${executable_with_build_number}
+                    '''
+                    }
+                }
+                
+            }
+        }
     }
 
     post {
