91eaee4fe56059ebb564ddce5d3c9176951b35c1
Paul Barbat <Paul.Barbat@Siriusxm.com>
2025-06-22 18:47:52 +0300

    Added resources folder to the package

--- jobs/build-cpp-66.groovy
+++ jobs/build-cpp-66.groovy
@@ -60,6 +60,7 @@ pipeline {
                         mkdir -p package_output
                         cp build/linux/Card_Game_66* package_output/
                         cp build/windows/Card_Game_66* package_output/
+                        cp -r resources/*.xml package_output/resources/
                         ls -ll build/linux
                         ls -ll build/windows
                         cd package_output
