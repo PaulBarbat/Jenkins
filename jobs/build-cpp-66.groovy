16527e358c202f07cb7b490c181ef601d21e7ec8
Paul Barbat <Paul.Barbat@Siriusxm.com>
2025-06-26 10:28:57 +0300

    Added branch parameter with the current dev branch

--- jobs/build-cpp-66.groovy
+++ jobs/build-cpp-66.groovy
@@ -1,6 +1,6 @@
 pipeline {
     parameters {
-    string(name: 'BRANCH', defaultValue: 'feature/Player', description: 'Git branch to build')
+    string(name: 'BRANCH', defaultValue: 'feature/GameState', description: 'Git branch to build')
     }
     agent { label 'jenkins-agent' }  // Ensure this label matches your autoscaling group nodes
 
